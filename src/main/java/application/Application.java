58b44c5c5776f4cf99e132933e15f3728f11b543
Petr <peter.92@yandex.ru>
2020-02-02 15:18:16 +0300

    - настроил liquibase plugin через отдельный файл

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
 @SpringBootApplication (scanBasePackages="application")
 @EntityScan(basePackages = "application.entity")
 @EnableJpaRepositories(basePackages = "application.dao")
