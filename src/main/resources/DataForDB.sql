b97121b89edb719d43f2eda23f307178be7395dc
Petr <peter.92@yandex.ru>
2020-02-16 17:37:23 +0300

    LiquiBase Final: - НЕ использвать всякие Liquibase плагины; все прекрасно работает через консоль; 1. сконфигурировать liquibase.properties 2. скачать jdbc драйвер для своей БД (MySql, Postgre и т.д.) и закинуть его в classpath 3. Запускать консоль liquibase из папки, где находится liquibase.properties 4. Liquibase использует .xml файл, указанный в liquibase.properties

--- src/main/resources/DataForDB.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO customer (FirstName, LastName, Age, Email, Address)
-values ('Tirion', 'Lanister', 25, 'BigBoy@lannister.com', 'RoyalHarbor'),
-       ('Jayme', 'Lanister', 30, 'KingKiller@lannister.com', 'RoyalHarbor'),
-       ('Sersea', 'Lanister', 27, 'LovelySister@lannister.com', 'RoyalHarbor');
--- src/main/resources/OnlineShopSqlScript2.sql
+++
@@ -1,9 +0,0 @@
-create schema if not exists hello;
-create table hello.customer (
-                          Id uuid,
-                          FirstName text,
-                          LastName text,
-                          Age INTEGER,
-                          Email text,
-                          Address text
-);
